a2f20d7fdab331e4cb7a256767d82f60a7047f83
dbjdbj <dbj@dbj.org>
2018-01-16 16:41:29 +0100

    created

--- dbj.cond.comparators/README.md
+++ dbj.cond.comparators/README.md
@@ -1,3 +1,22 @@
 ﻿# dbj.cond.comparators
 
+(c) 2009-2018 .. and beyond, by dbj.org  
+ Licensed under the MIT (MIT-LICENSE.txt) 
 
+<img src="http://dbj.org/wp-content/uploads/2013/07/comparator-e1373486780369.jpg" alt="dbj.cond() by dbj.org" width="100%" align="left" vspace="15px" />
+
+# &nbsp;
+
+Comparators to be used either standalone or with <a href="https://www.npmjs.com/package/dbj.cond" target="_blank">dbj.cond</a>
+
+For overview, usage with dbj.cond, tests (with dbj.cond too) and a such please go to the short <a href="https://github.com/DBJDBJ/dbj.cond/blob/master/man.md" target="_blank">**online manual**</a> on the GitHub project. 
+
+Please do feel free to send us your code and comments. 
+We might publish it and discuss it on https://dbj.org
+
+**NOTE**  
+ 
+Please do not hesitate to ask for a suport by mailing <a href="mailto:info@dbj.org" target="_blank">info@dbj.org</a>
+
+---------------------------------------------------------------------  
+### &copy; 2018 [![dbj();](http://dbj.org/wp-content/uploads/2015/12/cropped-dbj-icon-e1486129719897.jpg)](http://www.dbj.org "dbj")  
---
+++ dbj.cond.comparators/dbj.cond.comparators.js
@@ -0,0 +1,246 @@
+
+/*
+(c) dbj.org
+The absolute core of dbj cores ... perhaps we can call it dbj.core
+*/
+(function (undefined) {
+
+    /*
+    additions to ES5 intrinsics
+    */
+    /* moot point: what happens in the presence of another "".format() ? */
+    if ("function" != typeof "".format)
+        String.prototype.format = function () {
+            var args = arguments;
+            return this.replace(/\{(\d|\d\d)\}/g, function ($0) {
+                var idx = 1 * $0.match(/\d+/)[0]; return args[idx] !== undefined ? args[idx] : (args[idx] === "" ? "" : $0);
+            }
+            );
+        }
+
+    var oprot = Object.prototype, aprot = Array.prototype, sprot = String.prototype;
+
+    var /*implementation*/imp_ = {
+        /* coercion to Int32 as required by asm.js */
+        toInt32: function (v_) {
+            return v_ | 0;
+        },
+        isEven: function (value) { return (imp_.toInt32(value) % 2 == 0); },
+        /* dbj's type system */
+        type: (function () {
+            var rx = /\w+/g, tos = oprot.toString;
+            return function (o) {
+                if (typeof o === "undefined") return "undefined";
+                if (o === null) return "null";
+                if ("number" === typeof (o) && isNaN(o)) return "nan";
+                return (tos.call(o).match(rx)[1]).toLowerCase();
+            }
+        }()),
+        isObject: function (o) { return "object" === imp_.type(o); },
+        isFunction: function (o) { return "function" === imp_.type(o); },
+        isArray: function (o) { return "array" === imp_.type(o); },
+        isString: function (o) { return "string" === imp_.type(o); }
+    };
+
+    dbj.core = {
+
+        "toString": function () { return "dbj(); kernel 1.2.0"; },
+        /* 
+        coercion to Int32 
+        also required by asm.js
+        */
+        "toInt32": imp_.toInt32,
+        "isEven": imp_.isEven,
+
+        "oprot": oprot,
+        "aprot": aprot,
+        "sprot": sprot,
+
+        "type": imp_.type,
+        "isObject": imp_.isObject,
+        "isFunction": imp_.isFunction,
+        "isArray": imp_.isArray,
+        "isString": imp_.isString
+    };
+
+    // we do not export it to node from here
+    /*
+        if ('undefined' != typeof module ) module['exports'] = dbj ;
+    */
+
+}(
+    function () {
+        // for dom env this creates window.dbj
+        // for node env this creates module local var
+        if ("undefined" == typeof dbj)
+            dbj = {};
+        return dbj;
+    }()
+    ));
+
+/*--------------------------------------------------------------------------------------------*/
+/*
+MIT (c) dbj 2013 -2018 
+place for dbj comparators
+dependancy: dbj.kernel (above) and ES5
+NOTE: since 2013 quite a few comparators have been implemented please be sure which
+comparator you need and use the ones bellow after that.
+*/
+/*--------------------------------------------------------------------------------------------*/
+(function (dbj, undefined) {
+    "use strict";
+
+    // also defines what is a comparator : 
+    function strict_eq(a, b) { return a === b; }
+    // as per ES5 spec this returns false on different types
+
+    /*
+    find single in the array
+    only same types allowed to be compared 
+    (as customary) returns -1 , on not found
+    */
+    var index_of = function (array, searchElement, comparator) {
+        var found = -1;
+        array.every(
+            function (e, i) {
+                if (comparator(e, searchElement)) {
+                    found = i; return false;
+                };
+                return true;
+            });
+        return found;
+    };
+
+    /*
+    multi_comparator  allows arrays v.s. singles to be compared 
+    
+    Examples:
+    
+    multi_comparator( 1, [3,2,1] ) --> true
+    multi_comparator( [3,2,1], 1 ) --> true
+    multi_comparator( function (){ return 1;}, [3,2,1] ) --> false
+    multi_comparator( [3,2,1], ["x",[3,2,1]] ) --> true
+    
+    if some complex comparator is used then multi_comparator works for all types
+    */
+    var multi_comparator = function (a, b, comparator) {
+
+        if (!Array.isArray(a)) a = [a]; // 
+        if (!Array.isArray(b)) b = [b]; // 
+
+        if (comparator(a, b)) return true;          /* covers arr to arr too */
+        if (index_of(b, a, comparator) > -1) return true;            /* sing to arr */
+        if (index_of(a, b, comparator) > -1) return true;            /* arr to sing */
+
+        return false;
+    };
+
+    /*
+     Test for equality any JavaScript type. Also used in QUnit
+     equiv({a:1},{b:2}) --> true
+    
+     Discussions and reference: http://philrathe.com/articles/equiv
+     Test suites: http://philrathe.com/tests/equiv
+     Author: Philippe Rathé <prathe@gmail.com>
+    
+     LEGACY -- REMOVED -- does not work in presence of "use strict"
+     */
+
+
+    /*
+    Two arrays are considered equal when all their elements 
+    fulfill the following conditions:
+
+    1.  types are equal
+    2.  positions are equal
+    3. values are equal
+
+    Sparse arrays are also compared for equality
+
+    Solution using every() is fast because it uses native method for iteration
+    but it requires two way check since every will 'skip' over undefined entries
+    this checking [1,,2] vs [1,2] will be considered true.
+    
+    this is the tough test, that has to be satisfied:
+
+                 equal_arrays([1, 2, , 3], [1, 2, 3]); // => false
+    
+    function has(element, index) {
+        return this[index] === element;
+    }
+
+    function equal_arrays(a, b) {
+        return (a.length === b.length) && a.every(has, b) && b.every(has, a);
+    }
+    
+    optimised version of the above, also using the comparator
+    */
+    function equal_arrays(a, b, comparator) {
+
+        return (a.length === b.length) &&
+            a.every(function (e, i) { return comparator(e, b[i]); }) &&
+            b.every(function (e, i) { return comparator(e, a[i]); });
+    }
+
+    /* interface */
+    dbj.compare = {
+        'standard': strict_eq,
+        /* 
+        compare two arrays 
+       if comparator is given uses it otherwise uses strict_eq().
+
+       NOTE: this method is in here because it might prove faster than 
+       dbj.compare.multi()
+        */
+        'arr': function (a, b, /* optional */ comparator) {
+
+            if (!Array.isArray(a)) a = [a]; // throw TypeError("First argument must be array");
+            if (!Array.isArray(b)) b = [b]; // throw TypeError("Second argument must be array");
+
+            if (!!comparator && "function" != typeof comparator)
+                throw TypeError("Third argument is given but is not a function");
+
+            return equal_arrays(
+                a, b, comparator || strict_eq
+            )
+        },
+        /*
+        Can compare two arrays AND single to array AND array to single value
+        NOTE: if comparator is given use it otherwise use strict_eq().
+        */
+        'multi': function (a, b, comparator) {
+            return multi_comparator(a, b, comparator || strict_eq);
+        },
+        /*
+
+        LEGACY -- REMOVED -- does not work in presence of "use strict"
+        perform deep comparison of two objects or scalars
+        NOTE: to construct multi+deep comparator, end users will do this :
+
+         dbj.compare(a,b,dbj.compare.deep) ;
+
+        'deep': function (a, b) {
+            return rathe(a, b);
+        }
+
+        */
+    };
+
+    /*
+    export to Node.JS
+    (also works in the presence of qUnit "module")
+    */
+    if ("undefined" != typeof module) {
+        module['exports'] = dbj;  // for node js usage
+    }
+
+}(function () {
+    // for dom env this creates window.dbj
+    // for node env this creates module local var
+    if ("undefined" == typeof dbj)
+        dbj = {};
+    return dbj;
+}()
+    )
+);
+
